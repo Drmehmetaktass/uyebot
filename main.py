59a3eb6bfcc370bcfae76883af77f4729c70a1cb
Mehmet Aktaş <ma9034403@gmail.com>
2022-08-28 20:34:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ APP_ID = os.environ.get("APP_ID")
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 UPDATES_CHANNEL = "ailemizegel"
-OWNER= [1610383727 , 1137920946]
-PREMIUM= [1610383727 , 1137920946]
+OWNER= [1610383727 , 2104986532]
+PREMIUM= [1610383727 , 2104986532]
 app = pyrogram.Client("app", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 with open("data.csv", encoding='UTF-8') as f:
